202d1d7f42b2eecdf5552c28609dd59c7752c465
Pauline Ann Bautista <paulineannbautista@gmail.com>
2024-04-19 20:00:35 +0800

    updated js

--- script.js
+++ script.js
@@ -1,102 +1,102 @@
 // update navigation bar depending on the page
 function updateNavigation() {
-    // get position of each section and store in an array
-    const positions = [];
-    document.querySelectorAll('.contProgress a').forEach(function(link) {
-      const targetId = link.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      positions.push(targetElement.offsetTop);
-    });
-  
-    const scrollPosition = window.scrollY+100;
-  
-    positions.forEach(function(positions, index) {
-      const link = document.querySelectorAll('.contProgress a')[index];
-      if (scrollPosition >= positions) {
-        document.querySelectorAll('.contProgress a').forEach(function(link) {
-          link.classList.remove('selectedProg');
-        });
-        link.classList.add('selectedProg');
-      }
-    });
-  }
-  
-  // scroll to target section
+  // get position of each section and store in an array
+  const positions = [];
   document.querySelectorAll('.contProgress a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth'
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    positions.push(targetElement.offsetTop);
+  });
+
+  const scrollPosition = window.scrollY+100;
+
+  positions.forEach(function(positions, index) {
+    const link = document.querySelectorAll('.contProgress a')[index];
+    if (scrollPosition >= positions) {
+      document.querySelectorAll('.contProgress a').forEach(function(link) {
+        link.classList.remove('selectedProg');
       });
+      link.classList.add('selectedProg');
+    }
+  });
+}
+
+// scroll to target section
+document.querySelectorAll('.contProgress a').forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
     });
   });
-  
-  
-  window.addEventListener('scroll', function() {
-    updateNavigation();
+});
+
+
+window.addEventListener('scroll', function() {
+  updateNavigation();
+});
+
+
+// ------------------------------------------------------------------------------------------------
+// populate dropdown
+
+document.addEventListener("DOMContentLoaded", function() {
+  // fetch the nationalities data
+  fetch('./src/nationalities.txt')
+  .then(response => response.text())
+  .then(data => {
+    const nationalities = data.split('\n');
+
+    const dropdownNationality = document.getElementById("dropdownNationality");
+
+    // Populate the dropdown
+    for (let i = 0; i < nationalities.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = nationalities[i].trim();
+      dropdownNationality.appendChild(option);
+    }
+  })
+  .catch(error => {
+    console.error(error);
   });
-  
-  
-  // ----------------------------------------------------------------------
-  
-  document.addEventListener("DOMContentLoaded", function() {
-      // Fetch the nationalities data
-      fetch('./src/nationalities.txt')
-        .then(response => response.text())
-        .then(data => {
-          var nationalities = data.split('\n');
-          
-          // Get the dropdown element for nationality
-          var nationalityDropdown = document.getElementById("dropdownNationality");
-  
-          // Loop through the nationalities array and populate the dropdown
-          nationalities.forEach(nationality => {
-              var option = document.createElement("option");
-              option.textContent = nationality.trim(); // Remove any leading/trailing whitespace
-              nationalityDropdown.appendChild(option);
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
-  
-      // Fetch the data containing city and barangays
-      fetch('./src/ncr-list.json')
-        .then(response => response.json())
-        .then(data => {
-          var citiesDropdown = document.getElementById("dropdownCity");
-          var barangayDropdown = document.getElementById("dropdownBarangay");
-          
-          // Populate the cities dropdown
-          for (var city in data.cities) {
-              if (data.cities.hasOwnProperty(city)) {
-                  var option = document.createElement("option");
-                  option.textContent = city;
-                  citiesDropdown.appendChild(option);
-              }
+
+  // fetch the json containing city and barangays
+  fetch('./src/ncr-list.json')
+    .then(response => response.json())
+    .then(data => {
+      var dropdownCity = document.getElementById("dropdownCity");
+      var dropdownBarangay = document.getElementById("dropdownBarangay");
+
+      // get the cities in json and store in js variable
+      var cities = Object.keys(data.cities);
+
+      for (let i = 0; i < cities.length; i++) {
+        const option = document.createElement("option");
+        console.log(cities[i]);
+        option.textContent = cities[i];
+        dropdownCity.appendChild(option);
+      }
+
+      // get the selected city
+      dropdownCity.addEventListener("change", function() {
+          var selectedCity = dropdownCity.value;
+          var barangays = data.cities[selectedCity];
+
+          // clear the content of dropdown
+          dropdownBarangay.innerHTML = ""; 
+
+          // populate the barangay based on selected city
+          for (let i = 0; i < barangays.length; i++) {
+            const option = document.createElement("option");
+            option.textContent = barangays[i];
+            dropdownBarangay.appendChild(option);
           }
-  
-          // Event listener for city selection change
-          citiesDropdown.addEventListener("change", function() {
-              var selectedCity = citiesDropdown.value;
-              var barangays = data.cities[selectedCity];
-  
-              // Clear existing options in the barangay dropdown
-              barangayDropdown.innerHTML = "";
-  
-              // Populate the barangay dropdown with options based on the selected city
-              barangays.forEach(barangay => {
-                  var option = document.createElement("option");
-                  option.textContent = barangay;
-                  barangayDropdown.appendChild(option);
-              });
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
-  });
-  
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
